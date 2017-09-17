3dace6c4c5ccf35b34780c2731be0ad72bc1ff21
l1k3ab0t <thiesfronius@gmail.com>
2017-09-17 15:38:33 +0200

    Able to load simple html pages

--- Turney.go
+++ Turney.go
@@ -6,19 +6,30 @@ import (
 	"github.com/l1k3ab0t/GoJugg/lib/ReadConfig"
 	"log"
 	"strconv"
+	"net/http"
+	"html/template"
 )
 
 type settings struct {
+	name string
 	list      string
 	gameMode  int
 	groupCont int
+
 }
+
+//var tName string
+var setupDone   bool
+var templates = template.Must(template.ParseFiles("resources/control.html","resources/tournament.html","resources/setup.html"))
 
 func loadCFG() (settings, []GameEngine.Team) {
 	var s settings
 	var t []GameEngine.Team
 	cfg := ReadConfig.ReadFile("config.cfg")
 	for _, v := range cfg {
+		if ReadConfig.SplitConfig(v.Content)[0] == "TournamentName" {
+			s.name=ReadConfig.SplitConfig(v.Content)[1]
+		}
 		if ReadConfig.SplitConfig(v.Content)[0] == "List" {
 			s.list = ReadConfig.SplitConfig(v.Content)[1]
 		}
@@ -53,7 +64,38 @@ func loadCFG() (settings, []GameEngine.Team) {
 	return s, t
 }
 
+func defaultConnection(w http.ResponseWriter, r *http.Request) {
+	if setupDone {
+		http.Redirect(w, r, "/tournament/", http.StatusFound)
+	}else {
+		http.Redirect(w, r, "/setup/", http.StatusFound)
+	}
+}
+
+func controlPage(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "control")
+	log.Printf("IP: " + r.RemoteAddr + " connected")
+}
+
+func setupPage(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "setup")
+	log.Printf("IP: " + r.RemoteAddr + " connected")
+}
+
+func mainPage(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "tournament")
+	log.Printf("IP: " + r.RemoteAddr + " connected")
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	err := templates.ExecuteTemplate(w, tmpl+".html", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
 func main() {
+	setupDone=false
 	s, t := loadCFG()
 	if s.gameMode == 0 {
 		tg := GameEngine.BuildGroups(s.groupCont, t)
@@ -71,4 +113,12 @@ func main() {
 		}
 	}
 
+	//tName=s.name
+
+	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("resources"))))
+	http.HandleFunc("/", defaultConnection)
+	http.HandleFunc("/control/", controlPage)
+	http.HandleFunc("/setup/", setupPage)
+	http.HandleFunc("/tournament/", mainPage)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
